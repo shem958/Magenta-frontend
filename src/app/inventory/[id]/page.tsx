8fe7475e926ee20a23adc3225cc9e3022f558b28
shem958 <shemgikunda37@gmail.com>
2025-02-04 18:57:52 +0300

    Add Inventory detail and dashboard pages

---
+++ src/app/inventory/[id]/page.tsx
@@ -0,0 +1,6 @@
+"use client";
+import InventoryDetail from "../../../components/inventory/InventoryDetail";
+
+export default function InventoryDetailPage() {
+  return <InventoryDetail />;
+}
---
+++ src/app/inventory/page.tsx
@@ -0,0 +1,6 @@
+"use client";
+import InventoryDashboard from "../../components/inventory/InventoryDashboard";
+
+export default function InventoryPage() {
+  return <InventoryDashboard />;
+}
