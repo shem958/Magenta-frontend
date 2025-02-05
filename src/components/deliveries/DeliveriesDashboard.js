6cdccfa9c37e16373316c632a626800ac932c576
shem958 <shemgikunda37@gmail.com>
2025-02-05 13:48:13 +0300

    Enhance DeliveriesDashboard component by adding loading spinner and wrapping deliveries list in a Paper component for better presentation

--- src/components/deliveries/DeliveriesDashboard.js
+++ src/components/deliveries/DeliveriesDashboard.js
@@ -2,7 +2,13 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchDeliveries } from "../../store/slices/deliveriesSlice";
-import { Container, Typography } from "@mui/material";
+import {
+  Container,
+  Typography,
+  CircularProgress,
+  Box,
+  Paper,
+} from "@mui/material";
 import DeliveriesList from "./DeliveriesList";
 
 const DeliveriesDashboard = () => {
@@ -15,14 +21,27 @@ const DeliveriesDashboard = () => {
 
   return (
     <Container>
-      <Typography variant="h4" gutterBottom>
-        Deliveries Overview
-      </Typography>
-      {loading ? (
-        <Typography>Loading...</Typography>
-      ) : (
-        <DeliveriesList deliveries={deliveries} />
-      )}
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" gutterBottom>
+          Deliveries Overview
+        </Typography>
+        {loading ? (
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "50vh",
+            }}
+          >
+            <CircularProgress />
+          </Box>
+        ) : (
+          <Paper elevation={3} sx={{ p: 2 }}>
+            <DeliveriesList deliveries={deliveries} />
+          </Paper>
+        )}
+      </Box>
     </Container>
   );
 };
