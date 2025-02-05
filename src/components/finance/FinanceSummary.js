5226bc8f1cbc1c8858ad865b1471a6494985fdd5
shem958 <shemgikunda37@gmail.com>
2025-02-05 08:55:45 +0300

    Handle undefined finance data in FinanceSummary component by providing default values

--- src/components/finance/FinanceSummary.js
+++ src/components/finance/FinanceSummary.js
@@ -1,18 +1,18 @@
 "use client";
 import { Card, CardContent, Typography } from "@mui/material";
 
-const FinanceSummary = ({ data }) => {
+const FinanceSummary = ({ data = {} }) => {
   return (
     <Card>
       <CardContent>
         <Typography variant="h6">
-          Total Revenue: ${data.totalRevenue}
+          Total Revenue: ${data.totalRevenue || 0}
         </Typography>
         <Typography variant="h6">
-          Total Expenses: ${data.totalExpenses}
+          Total Expenses: ${data.totalExpenses || 0}
         </Typography>
         <Typography variant="h6">
-          Outstanding Debt: ${data.outstandingDebt}
+          Outstanding Debt: ${data.outstandingDebt || 0}
         </Typography>
       </CardContent>
     </Card>
