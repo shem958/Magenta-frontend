c5d2e17d28b21d7cff5e54b27fc7153167f244bb
shem958 <shemgikunda37@gmail.com>
2025-02-05 13:44:54 +0300

    Enhance Footer component by adding quick links and social media links, and updating layout with Grid2

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,12 +1,54 @@
 "use client";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Link, Container } from "@mui/material";
+import Grid2 from "@mui/material/Unstable_Grid2"; // Import Grid2
 
 const Footer = () => {
   return (
-    <Box sx={{ textAlign: "center", p: 2, bgcolor: "#f1f1f1" }}>
-      <Typography variant="body2">
-        &copy; {new Date().getFullYear()} Magenta. All rights reserved.
-      </Typography>
+    <Box sx={{ bgcolor: "#333", color: "#fff", py: 4 }}>
+      <Container maxWidth="lg">
+        <Grid2 container spacing={4}>
+          <Grid2 xs={12} sm={4}>
+            <Typography variant="h6" gutterBottom>
+              Magenta
+            </Typography>
+            <Typography variant="body2">
+              &copy; {new Date().getFullYear()} Magenta. All rights reserved.
+            </Typography>
+          </Grid2>
+          <Grid2 xs={12} sm={4}>
+            <Typography variant="h6" gutterBottom>
+              Quick Links
+            </Typography>
+            <Link href="/about" color="inherit" underline="none">
+              About Us
+            </Link>
+            <br />
+            <Link href="/contact" color="inherit" underline="none">
+              Contact Us
+            </Link>
+            <br />
+            <Link href="/privacy" color="inherit" underline="none">
+              Privacy Policy
+            </Link>
+          </Grid2>
+          <Grid2 xs={12} sm={4}>
+            <Typography variant="h6" gutterBottom>
+              Follow Us
+            </Typography>
+            <Link href="https://facebook.com" color="inherit" underline="none">
+              Facebook
+            </Link>
+            <br />
+            <Link href="https://twitter.com" color="inherit" underline="none">
+              Twitter
+            </Link>
+            <br />
+            <Link href="https://instagram.com" color="inherit" underline="none">
+              Instagram
+            </Link>
+          </Grid2>
+        </Grid2>
+      </Container>
     </Box>
   );
 };
