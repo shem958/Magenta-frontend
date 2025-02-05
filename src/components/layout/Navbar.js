e7065650b68b974b3b6083ed94b0277195528591
shem958 <shemgikunda37@gmail.com>
2025-02-05 13:41:09 +0300

    Enhance Navbar component by adding menu and notification icons, and a login button

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,11 +1,40 @@
 "use client";
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Button,
+  Box,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import NotificationsIcon from "@mui/icons-material/Notifications";
 
 const Navbar = () => {
   return (
-    <AppBar position="static">
+    <AppBar position="static" color="primary">
       <Toolbar>
-        <Typography variant="h6">Magenta</Typography>
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          Magenta
+        </Typography>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <IconButton color="inherit">
+            <NotificationsIcon />
+          </IconButton>
+          <IconButton color="inherit">
+            <AccountCircle />
+          </IconButton>
+          <Button color="inherit">Login</Button>
+        </Box>
       </Toolbar>
     </AppBar>
   );
