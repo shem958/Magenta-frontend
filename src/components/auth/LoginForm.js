928e422238e09766c0cad0b41a63e3532b76f66f
shem958 <shemgikunda37@gmail.com>
2026-03-05 14:34:38 +0300

    feat: add @hookform/resolvers and yup for form validation support

---
+++ src/components/auth/LoginForm.js
@@ -0,0 +1,106 @@
+"use client";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { loginSchema } from "../../lib/validations";
+import { useDispatch, useSelector } from "react-redux";
+import { login, clearError } from "../../store/slices/authSlice";
+import {
+  Box,
+  TextField,
+  Button,
+  Typography,
+  CircularProgress,
+  Alert,
+  Paper,
+  Container,
+} from "@mui/material";
+import { useEffect } from "react";
+
+const LoginForm = ({ onSuccess }) => {
+  const dispatch = useDispatch();
+  const { loading, error, isAuthenticated } = useSelector((state) => state.auth);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  useEffect(() => {
+    // Clear any previous errors when component mounts
+    dispatch(clearError());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (isAuthenticated && onSuccess) {
+      onSuccess();
+    }
+  }, [isAuthenticated, onSuccess]);
+
+  const onSubmit = async (data) => {
+    dispatch(login(data));
+  };
+
+  return (
+    <Container maxWidth="sm">
+      <Paper elevation={3} sx={{ p: 4, mt: 8 }}>
+        <Typography variant="h5" gutterBottom align="center">
+          Login
+        </Typography>
+
+        {error && (
+          <Alert severity="error" sx={{ mb: 2 }}>
+            {error}
+          </Alert>
+        )}
+
+        <Box
+          component="form"
+          onSubmit={handleSubmit(onSubmit)}
+          noValidate
+          sx={{ mt: 1 }}
+        >
+          <TextField
+            {...register("email")}
+            margin="normal"
+            fullWidth
+            label="Email Address"
+            autoComplete="email"
+            autoFocus
+            error={!!errors.email}
+            helperText={errors.email?.message}
+          />
+
+          <TextField
+            {...register("password")}
+            margin="normal"
+            fullWidth
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+            error={!!errors.password}
+            helperText={errors.password?.message}
+          />
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            disabled={loading}
+          >
+            {loading ? <CircularProgress size={24} /> : "Sign In"}
+          </Button>
+        </Box>
+      </Paper>
+    </Container>
+  );
+};
+
+export default LoginForm;
