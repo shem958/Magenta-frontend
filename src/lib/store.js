40447a30be7f59dcdd635380aa6db2bea99e1b64
shem958 <shemgikunda37@gmail.com>
2026-03-05 14:33:14 +0300

    feat: add auth reducer to the Redux store configuration

--- src/lib/store.js
+++ src/lib/store.js
@@ -3,9 +3,11 @@ import inventoryReducer from "../store/slices/inventorySlice";
 import employeesReducer from "../store/slices/employeesSlice";
 import financeReducer from "../store/slices/financeSlice";
 import deliveriesReducer from "../store/slices/deliveriesSlice";
+import authReducer from "../store/slices/authSlice";
 
 const store = configureStore({
   reducer: {
+    auth: authReducer,
     inventory: inventoryReducer,
     employees: employeesReducer,
     finance: financeReducer,
